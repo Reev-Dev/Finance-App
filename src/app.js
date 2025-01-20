bf89680eda966b07cc157e5c3ba2579ebb1ac918
attrfai <attarrifaismktibazma@gmail.com>
2025-01-20 11:12:47 +0700

    First initialization

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// import library yang dibutuhkan
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// konfigurasi dotenv agar membaca variabel lingkungan dari .env
+dotenv.config();
+
+// Inisialisasi express
+const app = express();
+
+// Middleware untuk parsing JSON pada request body
+app.use(express.json());
+
+// Port aplikasi
+const PORT = process.env.PORT || 5000;
+
+// Run server
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`);
+});
