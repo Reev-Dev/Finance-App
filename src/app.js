7ebbebc6138ae760537e37ef5e94b804d44503fc
attrfai <attarrifaismktibazma@gmail.com>
2025-02-03 14:56:01 +0700

    Add Fitur Reminder

--- src/app.js
+++ src/app.js
@@ -15,10 +15,12 @@ app.use(express.json());
 // Import Rute API
 const userRoutes = require('./routes/userRoutes');
 const financeRoutes = require('./routes/financeRoutes');
+const reminderRoutes = require('./routes/reminderRoutes');
 
 // Gunakan Rute API
 app.use('/api/users', userRoutes);
 app.use('/api/finances', financeRoutes);
+app.use('/api/reminders', reminderRoutes);
 
 // Port aplikasi
 const PORT = process.env.PORT || 5000;
