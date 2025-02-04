6fb2654f07f6e0d3a32a18d86e6840433d379038
attrfai <attarrifaismktibazma@gmail.com>
2025-02-04 11:50:10 +0700

    Configuration to connect MongoDB Atlas

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 5000,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (err) {
